ef3ca7f4aa02aec6f94df642d78b0a7b68f429a5
aerocy01 <163655245+aerocy01@users.noreply.github.com>
2024-12-21 19:06:43 +0800

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,17 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 class Queue:
     def __init__(self):
-        self.queue = []
-
-    def enqueue(self, data):
-        self.queue.append(data)
-
-    def dequeue(self):
-        if not self.is_empty():
-            return self.queue.pop(0)
-        return None  # If the queue is empty
+        self.front = self.rear = None
 
     def is_empty(self):
-        return len(self.queue) == 0
+        return self.front is None
+
+    def enqueue(self, item):
+        new_node = Node(item)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            return
+        self.rear.next = new_node
+        self.rear = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Dequeue from empty queue")
+        temp = self.front
+        self.front = temp.next
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.front.data
 
     def display(self):
-        return self.queue
+        elements = []
+        temp = self.front
+        while temp:
+            elements.append(temp.data)
+            temp = temp.next
+        return elements
